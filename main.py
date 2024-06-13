42fde097e33073ef442c2e71e917e16b4e2187c8
hien <hienmanhng@gmail.com>
2024-06-13 23:32:33 +0700

    Mentor-Ex02-renew

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
 import random
 # tính can chi, bài tập số 1 gửi mentor
-
-
 def calculate_can_chi(year):
     # tính can
     if year % 10 == 0:
@@ -55,3 +53,13 @@ def calculate_can_chi(year):
 
 
 calculate_can_chi(1990)
+
+# Bài tập số 2 gửi Mentor PTK
+# Bài toán yêu cầu tính tiền nhận được khi gửi 1$ trong 1 năm, với lãi suất 1 ngày là 1/365
+# sử dụng cấu trúc for loop để thực hiện yêu cầu
+def calculate_interest(money, period):
+  result = money
+  for i in range(period):
+    result = result * (1 + 1/period)
+  return result
+print(calculate_interest(1, 12), calculate_interest(1, 365), calculate_interest(1, 730), sep = " | ")
