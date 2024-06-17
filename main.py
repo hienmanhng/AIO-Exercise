8a81103115d1799b593e009660b5d75c6bf80884
hien <hienmanhng@gmail.com>
2024-06-17 16:34:59 +0700

    update newton_sqrt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
 import random
 # tính can chi, bài tập số 1 gửi mentor
+
+
 def calculate_can_chi(year):
     # tính can
     if year % 10 == 0:
@@ -57,9 +59,29 @@ calculate_can_chi(1990)
 # Bài tập số 2 gửi Mentor PTK
 # Bài toán yêu cầu tính tiền nhận được khi gửi 1$ trong 1 năm, với lãi suất 1 ngày là 1/365
 # sử dụng cấu trúc for loop để thực hiện yêu cầu
+
+
 def calculate_interest(money, period):
-  result = money
-  for i in range(period):
-    result = result * (1 + 1/period)
-  return result
-print(calculate_interest(1, 12), calculate_interest(1, 365), calculate_interest(1, 730), sep = " | ")
+    result = money
+    for i in range(period):
+        result = result * (1 + 1/period)
+    return result
+
+
+print(calculate_interest(1, 12), calculate_interest(
+    1, 365), calculate_interest(1, 730), sep=" | ")
+
+
+# Bài tập 3 gửi mentor
+# dùng phương pháp newton để tính căn bậc 2 cho 1 số dương a, với n là số lần cải thiện xấp xỉ, epsilon = 0.001
+
+def newton_sqrt(a, n, epsilon):
+    x = a
+    for i in range(n):
+        x = x - (x * x - a) / (2 * x)
+        if abs(x * x - a) < epsilon:
+            return x
+    return None
+
+
+print(newton_sqrt(a=10, n=100, epsilon=0.001))
